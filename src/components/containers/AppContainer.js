b29737a4707baa15a8d59185dc7c33cbd90f32f8
George Banis <gbanis@hubspot.com>
2016-10-22 19:58:10 -0400

    move stuff to old and add semicolons

--- src/components/containers/AppContainer.js
+++ src/components/containers/AppContainer.js
@@ -1,6 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import { browserHistory, Router } from 'react-router';
-import { Provider } from 'react-redux';
 
 class AppContainer extends Component {
   static propTypes = {
@@ -13,14 +11,10 @@ class AppContainer extends Component {
   }
 
   render () {
-    const { routes, store } = this.props;
-
     return (
-      <Provider store={store}>
-        <div style={{ height: '100%' }}>
-          <Router history={browserHistory} children={routes} />
-        </div>
-      </Provider>
+      <div style={{ height: '100%' }}>
+        hi
+      </div>
     );
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import createStore from './store/createStore';
-import AppContainer from './containers/AppContainer';
+// import createStore from './store/createStore'
+import AppContainer from './components/containers/AppContainer';
 
 // ========================================================
 // Store Instantiation
 // ========================================================
-const initialState = window.___INITIAL_STATE__;
-const store = createStore(initialState);
+// const initialState = window.___INITIAL_STATE__
+// const store = createStore(initialState)
 
 // ========================================================
 // Render Setup
@@ -15,10 +15,15 @@ const store = createStore(initialState);
 const MOUNT_NODE = document.getElementById('root');
 
 let render = () => {
-  const routes = require('./routes/index').default(store);
+  // const routes = require('./routes/index').default(store)
+
+  // ReactDOM.render(
+  //   <AppContainer store={store} routes={routes} />,
+  //   MOUNT_NODE
+  // )
 
   ReactDOM.render(
-    <AppContainer store={store} routes={routes} />,
+    <AppContainer />,
     MOUNT_NODE
   );
 };
